fa53c127618a3b9918a3e4e2ec988b3a9a6d5987
Marcos Hernan Garcia <marcos.h.garcia@gmail.com>
2026-04-10 01:17:36 -0300

    refactor(presentation): Adapt dependencies for async sessions

--- presentation/dependencies.py
+++ presentation/dependencies.py
@@ -1,4 +1,5 @@
 from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from application import ConstructService
 from persistence import ConstructDatastore
@@ -7,7 +8,7 @@ from presentation.graphql.schema import GraphQLContext
 
 
 def get_construct_datastore(
-    session=Depends(get_db_session),
+    session: AsyncSession = Depends(get_db_session),
 ) -> ConstructDatastore:
     return ConstructDatastore(session)
 
@@ -22,4 +23,3 @@ def get_graphql_context(
     service: ConstructService = Depends(get_construct_service),
 ) -> GraphQLContext:
     return GraphQLContext(construct_service=service)
-
