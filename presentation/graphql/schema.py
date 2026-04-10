5b9914287386af46d6f43c7469bef7a027fc5612
Marcos Hernan Garcia <marcos.h.garcia@gmail.com>
2026-04-10 16:32:35 -0300

    feat(graphql): Introduce ConstructSequenceFilterInput and update resolver

--- presentation/graphql/schema.py
+++ presentation/graphql/schema.py
@@ -3,6 +3,7 @@ from strawberry.fastapi import BaseContext
 from strawberry.types import Info
 
 from application import ConstructService
+from domain import ConstructSequenceFilter
 from presentation.graphql.types import Construct
 
 
@@ -12,18 +13,28 @@ class GraphQLContext(BaseContext):
         self.construct_service = construct_service
 
 
+@strawberry.input
+class ConstructSequenceFilterInput:
+    gene_name: str
+    min_length: int | None = None
+
+    def to_domain(self) -> ConstructSequenceFilter:
+        return ConstructSequenceFilter(
+            gene_name=self.gene_name,
+            min_length=self.min_length,
+        )
+
+
 @strawberry.type
 class Query:
     @strawberry.field
     async def constructs_by_sequence(
         self,
         info: Info[GraphQLContext, None],
-        gene_name: str,
-        min_length: int | None = None,
+        filters: ConstructSequenceFilterInput,
     ) -> list[Construct]:
         constructs = await info.context.construct_service.list_constructs_by_sequence(
-            gene_name=gene_name,
-            min_length=min_length,
+            filters=filters.to_domain(),
         )
         return [Construct.from_domain(construct) for construct in constructs]
 
